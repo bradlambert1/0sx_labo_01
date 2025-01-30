673943b774e9c0278b69bf23aba290dfc4082129
Merulox H <bradlambert@live.ca>
2025-01-30 16:53:47 -0500

    things

--- labo01/labo01.ino
+++ labo01/labo01.ino
@@ -1,4 +1,4 @@
-const string ID = "2471005";
+const String ID = "2471005";
 
 void setup() {
   // put your setup code here, to run once:
@@ -6,21 +6,22 @@ void setup() {
 }
 
 void etatAllume() {
-serial.print("Allume - ");
-serial.print(ID);
+Serial.print("Allume - ");
+Serial.print(ID);
 digitalWrite(LED_BUILTIN, HIGH);
 delay(2000);
 digitalWrite(LED_BUILTIN, LOW);
 }
 
 void etatClignotement() {
-serial.print("Clignotement - ");
-serial.print(ID);
-int loops = stoi(ID[-2]);
-if (loops == 0) {
-  loops = 10;
+Serial.print("Clignotement - ");
+Serial.print(ID);
+char loops = ID[-2];
+int loopsN = 0;
+if (loops == '0') {
+  loopsN = 10;
 }
-for (int i = 0; i<loops; i++) {
+for (int i = 0; i<loopsN; i++) {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(250);
   digitalWrite(LED_BUILTIN, LOW);
@@ -29,31 +30,32 @@ for (int i = 0; i<loops; i++) {
 }
 
 void etatVariation() {
-serial.print("Variation - ");
-serial.print(ID);
+Serial.print("Variation - ");
+Serial.print(ID);
 float brightness = 0;
-if (stoi(ID[-1]) % 2 == 0 ) {
+if (ID[-1] == '2' || ID[-1] == '4' || ID[-1] == '6' || ID[-1] == '8' || ID[-1] == '0') {
   brightness = 0;
   for (int i = 0; i<128; i++) {
-      brightness += 0.78125
+      brightness += 0.78125;
     delay(16);
   }
 } else {
   brightness = 100;
   for (int i = 0; i<128; i++) {
-      brightness -= 0.78125
+      brightness -= 0.78125;
     delay(16);
   }
 
   brightness = 100;
+  }
 }
 
 void etatEteint() {
-  serial.print("Eteint - ");
-  serial.print(ID);
+  Serial.print("Eteint - ");
+  Serial.print(ID);
   digitalWrite(LED_BUILTIN, LOW);
   delay(2000);
-  digitalWrite(LED_BUILTIN, high);
+  digitalWrite(LED_BUILTIN, HIGH);
 
 }
 void loop() {
